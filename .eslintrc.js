93d87de4ceea7c8636f7bb31c53d0d0efd83f0d6
Romain Racamier-Lafon <romain.racamier@gmail.com>
2020-02-08 09:20:56 +0100

    add custom eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,14 @@
+const rules = require('./.eslintrc.rules.js')
+
 module.exports = {
-  env: {
-    browser: true,
-    es6: true
-  },
   extends: [
     'standard'
   ],
-  globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly'
-  },
   parserOptions: {
     ecmaVersion: 2018
   },
-  rules: {
-  }
+  plugins: [
+    'html'
+  ],
+  rules
 }
