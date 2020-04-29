61ddf68af5becda754051d044e8be4b77269b97a
Romain Racamier-Lafon <romain.racamier@gmail.com>
2020-04-29 16:24:07 +0200

    add spotify & amazon exporters

---
+++ src/mb-import-from-amazon-music.user.js
@@ -0,0 +1,44 @@
+// ==UserScript==
+// @name        Amazon Music - Export to MusicBrainz
+// @namespace   https://github.com/Shuunen
+// @match       https://www.amazon.fr/*
+// @grant       none
+// @version     1.0.0
+// @require     https://raw.githubusercontent.com/Shuunen/user-scripts/master/src/mb-import-utils.js
+// @author      Shuunen
+// @description This script let you import releases on Amazon Music to the great MusicBrainz db <3
+// ==/UserScript==
+
+/* global textFromSelector, insertMbForm */
+
+function mbImport () {
+  const getTracks = () => Array.from(document.querySelectorAll('[id^="dmusic_tracklist_player_row"]')).map((el, index) => ({
+    number: (index + 1) + '',
+    name: textFromSelector('.TitleLink', el),
+    length: textFromSelector('[id^="dmusic_tracklist_duration"]', el),
+  }))
+  const details = textFromSelector('#productDetailsTable')
+  const data = {
+    app: {
+      id: 'mb-import-from-amz-music',
+      title: 'Amazon to MB',
+    },
+    title: textFromSelector('[data-feature-name="dmusicProductTitle"]'),
+    artist: textFromSelector('[data-feature-name="artistLink"'),
+    date: { year: 0, month: 0, day: 0 },
+    label: (details.match(/Label: (\S+)/) || [])[1] || '',
+    url: document.location.href,
+    urlType: '77', // amazon link
+    tracks: getTracks(),
+  }
+  const dateMatches = details.match(/origine : (\d{1,2}) (\S+) (\d{4})/) || []
+  if (dateMatches.length === 4) {
+    const months = ['janvier', 'février', 'mars', 'avril', 'mai', 'juin', 'juillet', 'août', 'septembre', 'octobre', 'novembre', 'décembre']
+    data.date.year = dateMatches[3]
+    data.date.month = months.indexOf(dateMatches[2]) + 1
+    data.date.day = dateMatches[1]
+  }
+  insertMbForm(data)
+}
+
+window.addEventListener('load', () => mbImport())
---
+++ src/mb-import-from-spotify.user.js
@@ -0,0 +1,44 @@
+// ==UserScript==
+// @name        Spotify - Export to MusicBrainz
+// @namespace   https://github.com/Shuunen
+// @match       https://open.spotify.com/album/*
+// @match       https://open.spotify.com/playlist/*
+// @grant       none
+// @require     https://raw.githubusercontent.com/Shuunen/user-scripts/master/src/mb-import-utils.js
+// @version     1.0.0
+// @author      Shuunen
+// @description This script let you import releases on Spotify to the great MusicBrainz db <3
+// ==/UserScript==
+
+/* global textFromSelector, insertMbForm */
+
+function mbImport () {
+  const getTracks = () => Array.from(document.querySelectorAll('.tracklist-row')).map((el, index) => ({
+    number: (index + 1) + '',
+    name: textFromSelector('.tracklist-name', el),
+    artists: textFromSelector('.tracklist-row__artist-name-link', el),
+    duration: textFromSelector('.tracklist-duration', el),
+  }))
+  const data = {
+    app: {
+      id: 'mb-import-from-spotify',
+      title: 'Spotify to MB',
+    },
+    title: textFromSelector('h1'),
+    artist: textFromSelector('a[href^="/artist/"]'),
+    date: { year: 0, month: 0, day: 0 },
+    label: (textFromSelector('.copyrights p').match(/\d{4}\s(\w+)/) || [])[1] || '',
+    url: document.location.href,
+    urlType: '85', // music streaming link
+    tracks: getTracks(),
+  }
+  const dateMatches = textFromSelector('script[type="application/ld+json"]').match(/datePublished":"(\d{4})-(\d{2})-(\d{2})/) || []
+  if (dateMatches.length === 4) {
+    data.date.year = dateMatches[1]
+    data.date.month = dateMatches[2]
+    data.date.day = dateMatches[3]
+  }
+  insertMbForm(data)
+}
+
+window.addEventListener('load', () => mbImport())
