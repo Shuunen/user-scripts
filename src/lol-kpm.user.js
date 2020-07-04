2a5f7cc1105d885289836fda91bbb504c485a811
Romain Racamier-Lafon <romain.racamier@gmail.com>
2020-07-04 10:29:50 +0200

    set max kpm to 1.8

--- src/lol-kpm.user.js
+++ src/lol-kpm.user.js
@@ -1,12 +1,16 @@
 // ==UserScript==
-// @name        LoL - KPM Counter Kills per minutes
+// @name        LoL - KPM Counter - Kills per minutes
 // @namespace   https://github.com/Shuunen
 // @description Show how many kills per minutes you did, isn't the title that obvious ?
 // @author      Romain Racamier-Lafon
 // @match       https://matchhistory.euw.leagueoflegends.com/*
 // @grant       none
-// @version     1.0
+// @version     1.1
 // ==/UserScript==
+
+const maxKpm = 1.8
+
+const coolPercents = kpm => Math.min(Math.max(Math.round(kpm / maxKpm * 100), 0), 100)
 
 const showKpmOnRow = (row) => {
   let kills = null
@@ -17,15 +21,15 @@ const showKpmOnRow = (row) => {
   if (!min) return
   const el = document.createElement('div')
   const kpm = Math.round(kills / min * 10) / 10
-  const wowPercent = Math.min(Math.max(Math.round(0.5 * kpm * 100), 0), 100) // 5% ur bad, 100% super good
+  const score = coolPercents(kpm) // 5% ur bad, 100% super good
   el.textContent = `${kpm} kills/min`
-  el.title = `You're ${wowPercent}% awesome`
+  el.title = `You're ${score}% awesome`
   el.style = `
     white-space: nowrap;
     padding-right: 15px;
     font-size: ${kpm * 10 + 5}px;
-    color: hsl(120, 100%, ${wowPercent}%);
-    background-color: hsl(0, 0%, ${100 - wowPercent}%);
+    color: hsl(120, 100%, ${score}%);
+    background-color: hsl(0, 0%, ${100 - score}%);
   `
   row.appendChild(el)
   row.classList.add('kpm-handled')
