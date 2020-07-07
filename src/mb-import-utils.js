cee6b60ccffa817aef2fdf7c0148e88b6f3e9311
jesus2099 <jesus2099@users.noreply.github.com>
2020-07-07 19:22:21 +0200

    Fix broken link to user script code on GitHub

--- src/mb-import-utils.js
+++ src/mb-import-utils.js
@@ -67,7 +67,7 @@ const insertMbForm = ({ app, title, artist, date, tracks, label, url, urlType })
   addMbField(form, 'labels.0.name', label)
   addMbField(form, 'urls.0.url', url, true)
   addMbField(form, 'urls.0.link_type', urlType, true)
-  addMbField(form, 'edit_note', `Release ${url} imported using github.com/Shuunen/user-scripts/src/${app.id}.user.js`, true)
+  addMbField(form, 'edit_note', `Release ${url} imported using github.com/Shuunen/user-scripts/blob/master/src/${app.id}.user.js`, true)
   addMbSubmit(form)
   document.body.appendChild(form)
 }
