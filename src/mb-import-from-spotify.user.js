952e20bec8c0bb0717899d4dfff1e3c7101fbce3
jesus2099 <jesus2099@users.noreply.github.com>
2020-07-09 23:35:45 +0200

    Fix spotify (#20)
    * Find label, track names and track durations
    
    Tested on https://open.spotify.com/album/6fg7kbar0InuDku7sv8xkB with
    Chromium 78.0.3904.108, Raspbian 10 (32 bits)
    
    * Follow library change
    
    I changed mb-import-utils.js library in aada890a2cfe3a8d037e0cdaf9f7a246446e0161 because its `track.length` was not consistent with Spotify importer's `track.duration`.
    Between `length` and `duration` I chose duration as length has a meaning in JavaScript.
    
    So now that Spotify and library are OK, I change Amazon, the only other script using this library.
    
    * Add Vivaldi and Firefox support
    
    Current script works on Chromium simplified Spotify website (only album tracklist).
    I am adding support for full-fledged Spotify website (deferred loading, playlist sidebar, media player) that is served to Vivaldi and Firefox (at least).
    
    * Fix label fetch for copyrights with no copyright year
    
    Current script worked with https://open.spotify.com/album/6fg7kbar0InuDku7sv8xkB
    © 2019 Jones/Tintoretto Entertainment Company LLC
    ℗ 2019 Jones/Tintoretto Entertainment Company LLC under exclusive licence to Parlophone Records Ltd, a Warner Music Group Company
    
    Now it also works with https://open.spotify.com/album/0skg1dUrsqiN21tfxC8a52
    ℗ AVEX MUSIC CREATIVE INC.
    
    BTW IMO it's incorrect to take these as release labels,
    they are © copyright and ℗ phonographic copyright relationships.
    
    * Fix track artist fetching
    
    A typo prevented fetching track artists
    for [various artists] compilations like
    https://open.spotify.com/album/50XXvvpejii6nYcoomU0LV
    
    * Show reference used for all edits
    
    * Fix broken link to user script code on GitHub
    
    * VA compilation fix for simplified Spotify pages
    
    Tested on https://open.spotify.com/album/50XXvvpejii6nYcoomU0LV
    with Chromium 78.0.3904.108, Raspbian 10 (32 bits)
    
    * JavaScript validation fix
    
    Trying to pass the quality test.
    See https://github.com/Shuunen/user-scripts/pull/20#issuecomment-655693684
    
    * Code convention by `npm test` command
    
    Strange thing is that this npm thing insists on removing some semicolons
    ; from the code.
    For me it's not the cleanest thing to do but I guess, if npm is to be
    used in this repo, I should commit that, anyway.
    
    * Bump versions
    
    Now 1.0.1 has been used on master branch

--- src/mb-import-from-spotify.user.js
+++ src/mb-import-from-spotify.user.js
@@ -5,7 +5,7 @@
 // @match       https://open.spotify.com/playlist/*
 // @grant       none
 // @require     https://raw.githubusercontent.com/Shuunen/user-scripts/master/src/mb-import-utils.js
-// @version     1.0.1
+// @version     1.0.2
 // @author      Shuunen
 // @description This script let you import releases on Spotify to the great MusicBrainz db <3
 // ==/UserScript==
@@ -15,19 +15,19 @@
 function mbImport () {
   const getTracks = () => [...document.querySelectorAll('.tracklist-row')].map((element, index) => ({
     number: (index + 1) + '',
-    name: textFromSelector('.tracklist-name', element),
-    artists: textFromSelector('.tracklist-row__artist-name-link', element),
-    duration: textFromSelector('.tracklist-duration', element),
+    name: textFromSelector('.track-name, .tracklist-name', element),
+    artist: textFromSelector('.artists-inline, .tracklist-row__artist-name-link', element),
+    duration: textFromSelector('.total-duration, .tracklist-duration', element),
   }))
   const data = {
     app: {
       id: 'mb-import-from-spotify',
       title: 'Spotify to MB',
     },
-    title: textFromSelector('h1'),
-    artist: textFromSelector('a[href^="/artist/"]'),
+    title: textFromSelector('.entity-info.media h1, section.content h1'),
+    artist: textFromSelector('.entity-info.media h2 a, section.content a[href^="/artist/"]') || textFromSelector('.entity-info.media h2').match(/^.+\b\s(.+)$/)[1],
     date: { year: 0, month: 0, day: 0 },
-    label: (textFromSelector('.copyrights p').match(/\d{4}\s(.+)/) || [])[1] || '',
+    label: (textFromSelector('.copyrights li, section.content p[as="p"]').match(/[\d\s©℗]+(.*)/) || [])[1] || '',
     url: document.location.href,
     urlType: '85', // music streaming link
     tracks: getTracks(),
@@ -38,7 +38,10 @@ function mbImport () {
     data.date.month = dateMatches[2]
     data.date.day = dateMatches[3]
   }
-  insertMbForm(data)
+  if (data.title && data.artist) {
+    clearInterval(spotifyNotFullyLoaded)
+    insertMbForm(data)
+  }
 }
 
-window.addEventListener('load', () => mbImport())
+var spotifyNotFullyLoaded = setInterval(mbImport, 1000)
