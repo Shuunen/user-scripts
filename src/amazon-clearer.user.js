da1c91c0aa6633dd2c219b6b279944da5252f5dc
Romain Racamier-Lafon <romain.racamier@gmail.com>
2021-04-11 22:09:11 +0200

    lint 2

--- src/amazon-clearer.user.js
+++ src/amazon-clearer.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
-// @name         Amazon - Cleaner
+// @name         Amazon - Clearer
 // @namespace    https://github.com/Shuunen
-// @version      1.0.5
+// @version      1.0.6
 // @description  Un-clutter Amazon website
 // @author       Romain Racamier-Lafon
 // @match        https://www.amazon.fr/*
@@ -9,54 +9,46 @@
 // @grant        none
 // ==/UserScript==
 
-(function () {
-  /* global Shuutils */
-  'use strict'
-
-  const app = {
-    id: 'amz-clr',
-    debug: false,
-  }
-
+(function AmazonClearer() {
+  /* global document, Shuutils */
+  const utils = new Shuutils({ id: 'amz-clr', debug: false })
   const selectors = {
     productLine: '.s-item-container',
   }
-
   const uselessSelectors = {
     oldPrice: '.a-text-strike, #priceblock_saleprice_lbl, #vatMessage',
     badges: '.a-badge-label, #acBadge_feature_div, #zeitgeistBadge_feature_div, .dotdBadge, .a-row.DEAL_OF_THE_DAY',
-    ads: '.nav-swmslot, .GB-M-COMMON.GB-SHOVELER, #pdagSparkleAdFeedback, #detail-ilm_div, #quickPromoBucketContent, #sp_detail, #hqpWrapper, #productAlert_feature_div, #navSwmHoliday, #quickPromoDivId',
-    recommandations: '#raw-sitewide-rhf, #rhf',
-    instantBuy: '#buyNow_feature_div, #oneClick_feature_div',
+    ads: '.nav-swmslot, .GB-M-COMMON.GB-SHOVELER, #pdagSparkleAdFeedback, #detail-ilm_div, #quickPromoBucketContent, #sp_detail, #hqpWrapper, #productAlert_feature_div, #navSwmHoliday, #quickPromoDivId, [cel_widget_id^="adplacements"], [data-cel-widget^="percolate"], [data-cel-widget="nav_sitewide_msg"], [cel_widget_id^="dpx-sponsored"]',
+    recommandations: '#raw-sitewide-rhf, #rhf, [cel_widget_id^="tetris"], [cel_widget_id^="sims-consolidated"]',
+    instantBuy: '#buyNow_feature_div, #oneClick_feature_div, [data-cel-widget="secureTransaction_feature_div"]',
     dashButtons: '#digitalDashHighProminence_feature_div',
     sharing: '#tellAFriendBox_feature_div',
+    sellYours: '[data-cel-widget="moreBuyingChoices_feature_div"]',
     buyPack: '#sims-fbt',
     comparison: '#HLCXComparisonWidget_feature_div',
   }
-
-  const utils = new Shuutils(app)
-
-  function deleteUseless () {
+  function deleteUseless() {
     Object.keys(uselessSelectors).forEach(key => {
       utils.findAll(uselessSelectors[key], document, true).forEach(node => {
-        // node.style = 'background-color: red !important;color: white !important;'
-        node.remove()
+        if (utils.app.debug) node.style = 'background-color: red !important;color: white !important; box-shadow: 0 0 10px red;'
+        else node.remove()
       })
     })
   }
-
-  function cosmeticChanges () {
-    utils.findAll(selectors.productLine, document, true).forEach(node => (node.classList = []))
+  function cosmeticChanges() {
+    utils.findAll(selectors.productLine, document, true).forEach(node => {
+      node.className = ''
+    })
   }
-
-  function process () {
+  function process() {
     utils.log('processing')
     deleteUseless()
     cosmeticChanges()
   }
-
   process()
-
+  setTimeout(() => process(), 500)
+  setTimeout(() => process(), 1000)
+  setTimeout(() => process(), 1500)
   const processDebounced = utils.debounce(process, 500)
   document.addEventListener('scroll', processDebounced)
 })()
