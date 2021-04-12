f9e357783987eec224b76047be481e228acde652
Romain Racamier-Lafon <romain.racamier@gmail.com>
2021-04-12 18:05:15 +0200

    fix

--- src/mb-import-from-spotify.user.js
+++ src/mb-import-from-spotify.user.js
@@ -6,7 +6,7 @@
 // @grant       none
 // @require     https://raw.githubusercontent.com/Shuunen/user-scripts/master/src/mb-import-utils.js
 // @require     https://raw.githubusercontent.com/Shuunen/user-scripts/master/src/utils.js
-// @version     1.0.3
+// @version     1.0.4
 // @author      Shuunen
 // @description This script let you import releases on Spotify to the great MusicBrainz db <3
 // ==/UserScript==
@@ -14,19 +14,22 @@
 (function SpotifyMusicBrainzExport() {
   /* global document, textFromSelector, insertMbForm, Shuutils */
   const utils = new Shuutils({ id: 'spotify-mb-export', debug: false })
+  const selectors = {
+    title: '.entity-info.media h1, .os-content h1[as="h1"]',
+  }
   const getTracks = () => utils.findAll('.tracklist-row, [data-testid="tracklist-row"]').map((element, index) => ({
     number: String(index + 1),
     name: textFromSelector('.track-name, .tracklist-name, [as="div"]', element),
     artist: textFromSelector('.artists-inline, .tracklist-row__artist-name-link, a[href^="/artist/"]', element),
     duration: textFromSelector('.total-duration, .tracklist-duration, [as="div"][style]', element),
   }))
-  function mbImport() {
+  const mbImport = () => {
     const data = {
       app: {
         id: 'mb-import-from-spotify',
         title: 'Spotify to MB',
       },
-      title: textFromSelector('.entity-info.media h1, .os-content h1[as="h1"]'),
+      title: textFromSelector(selectors.title),
       artist: textFromSelector('.entity-info.media h2 a, .os-content a[href^="/artist/"]') || (textFromSelector('.entity-info.media h2').match(/^.+\b\s(.+)$/) || [])[1],
       date: { year: 0, month: 0, day: 0 },
       label: (textFromSelector('.copyrights li, .os-content p[as="p"]').match(/[\d\s©℗]+(.*)/) || [])[1] || '',
@@ -40,10 +43,13 @@
       data.date.month = dateMatches[2]
       data.date.day = dateMatches[3]
     }
-    if (data.title && data.artist) {
-      clearInterval(spotifyNotFullyLoaded)
-      insertMbForm(data)
-    }
+    if (data.title && data.artist) return insertMbForm(data)
+    utils.warn('cannot insert MB form without title & artist')
   }
-  const spotifyNotFullyLoaded = setInterval(mbImport, 1000)
+  const init = async () => {
+    const title = await utils.waitToDetect(selectors.title)
+    if (title === undefined) return utils.log('no music title found on this page')
+    mbImport()
+  }
+  utils.onPageChange(init)
 })()
