175373c4687cb39d743dd6add9588189cf29e3d9
Romain Racamier-Lafon <romain.racamier@gmail.com>
2021-04-26 15:18:55 +0200

    fix time parse

--- src/lol-kpm.user.js
+++ src/lol-kpm.user.js
@@ -6,7 +6,7 @@
 // @match       https://matchhistory.euw.leagueoflegends.com/*
 // @require     https://raw.githubusercontent.com/Shuunen/user-scripts/master/src/utils.js
 // @grant       none
-// @version     1.1.2
+// @version     1.1.3
 // ==/UserScript==
 
 (function LolKpmCounter() {
@@ -16,9 +16,10 @@
   const coolPercents = kpm => Math.min(Math.max(Math.round(kpm / maxKpm * 100), 0), 100)
   const showKpmOnRow = row => {
     const kills = Number.parseInt(row.querySelector('.kda-plate').textContent.split('/')[0], 10)
-    const min = Number.parseInt(row.querySelector('.date-duration').textContent.split(':')[0], 10)
+    const time = row.querySelector('.date-duration').textContent.split(' ')[0].split(':').map(string => Number.parseInt(string, 10))
+    const minutes = time.length === 3 ? ((60 * time[0]) + time[1]) : time[0]
     const element = document.createElement('div')
-    const kpm = Math.round(kills / min * 10) / 10
+    const kpm = Math.round(kills / minutes * 10) / 10
     const score = coolPercents(kpm) // 5% ur bad, 100% super good
     element.textContent = `${kpm} kills/min`
     element.title = `You're ${score}% awesome`
