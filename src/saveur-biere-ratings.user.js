7d3fc3449bbdfbd641cc8674636592c83e60b385
Romain Racamier-Lafon <romain.racamier@gmail.com>
2021-11-28 22:28:18 +0100

    cache results for a month

--- src/saveur-biere-ratings.user.js
+++ src/saveur-biere-ratings.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Saveur Bière - Untappd Ratings
 // @namespace    https://github.com/Shuunen
-// @version      1.1.1
+// @version      1.1.2
 // @description  See your ratings when buying
 // @author       Romain Racamier-Lafon
 // @match        https://www.saveur-biere.com/*
@@ -12,10 +12,11 @@
 (function SaveurBiereUntappd () {
   /* global Shuutils */
   const marker = 'svb-rat'
-  const utils = new Shuutils({ id: marker, debug: false })
-  const user = window.localStorage.untappdUser || ''
+  const cached = new Date().toISOString().substr(0, 7) // like 2021-11
+  const utils = new Shuutils({ id: marker, debug: true })
+  const user = localStorage.untappdUser || ''
   if (user === '') return utils.error('please set localStorage.untappdUser to use this script')
-  const wrapAPIKey = window.localStorage.wrapAPIKey || ''
+  const wrapAPIKey = localStorage.wrapAPIKey || ''
   if (wrapAPIKey === '') return utils.error('please set localStorage.wrapAPIKey to use this script')
   const selectors = {
     items: 'div[class^="styled__List"] > div[class^="styled__Container-sc"],[class^="styled__Products"] > div[class^="styled__Container-sc"], div[class^="styled__Content"] > h1[class^="styled__Title"]:first-child, div[class^="styled__Column-sc"] > span[class^="styled__Title-sc"]',
@@ -32,15 +33,28 @@
     else rating.style.backgroundColor = 'lightpink'
     element.append(rating)
     element.classList.add(marker)
+    element.parentElement.style.height = 'auto'
   }
   const fetchRating = async item => {
     const titleElement = item.childElementCount === 0 ? item : utils.findOne(selectors.title, item)
     if (titleElement === null) return utils.error('cant find item title')
-    const name = titleElement.textContent.split(' - ')[0].trim()
-    utils.log('fetching rating for :', name)
-    const data = await fetch(`https://wrapapi.com/use/jojo/untappd/history/0.0.1?user=${user}&search=${name}&wrapAPIKey=${wrapAPIKey}`).then(response => response.json()).then(response => response.data)
-    if (data === null) return utils.log(`no ratings found for item "${name}"`)
-    utils.log('found data', data, 'for item', name)
+    const name = utils.readableString(titleElement.textContent.split(' - ')[0].replace(' 75cl', '')).trim()
+    if (name === '') return utils.error('cant find item name')
+    const storageKey = `${marker}-${cached}-${name}` // '2021-11-svb-rat-Gouden Carolus Tripel'
+    utils.log(`looking for "${storageKey}" in localStorage`)
+    let data = {}
+    if (localStorage.getItem(storageKey) !== null) {
+      data = localStorage.getItem(storageKey)
+      utils.log(`found ${data === 'EMPTY' ? 'EMPTY ' : ''}cached data for "${storageKey}" in localStorage`)
+      if (data === 'EMPTY') return utils.log(`no ratings found for item "${name}"`)
+      data = JSON.parse(data)
+    } else {
+      utils.log('fetching rating for :', name)
+      data = await fetch(`https://wrapapi.com/use/jojo/untappd/history/0.0.1?user=${user}&search=${name}&wrapAPIKey=${wrapAPIKey}`).then(response => response.json()).then(response => response.data)
+      utils.log(`fetched ${data ? '' : 'EMPTY '}data`, data, 'for item', name)
+      localStorage.setItem(storageKey, data ? JSON.stringify(data) : 'EMPTY')
+      if (data === null) return utils.log(`no ratings found for item "${name}"`)
+    }
     injectRating(titleElement.parentElement, data)
     if (titleElement.tagName !== 'A') titleElement.outerHTML = `<a href="https://www.saveur-biere.com/fr/search-result/${name}" target="_blank">${name}</a>`
   }
