f4ea67335531062e1f21a3154186fee2047340b4
Romain Racamier-Lafon <romain.racamier@gmail.com>
2021-12-19 23:48:58 +0100

    improve npm package detection

--- src/bundlephobia-everywhere.user.js
+++ src/bundlephobia-everywhere.user.js
@@ -8,27 +8,30 @@
 // @match       https://www.npmjs.com/package/*
 // @grant       none
 // @require     https://raw.githubusercontent.com/Shuunen/user-scripts/master/src/utils.js
-// @version     1.0.8
+// @version     1.1.0
 // ==/UserScript==
 
 (function BundlePhobiaEverywhere () {
   /* global Shuutils */
   const utils = new Shuutils({ id: 'bdl-evr', debug: false })
   const injectBadge = async name => {
-    const url = `https://img.shields.io/bundlephobia/min/${name}`
-    const image = document.createElement('img')
-    image.src = url
-    image.style = 'position: absolute; right: 0; top: 1rem;'
+    const link = document.createElement('a')
+    link.innerHTML = `"${name}" stats from BundlePhobia :
+      <img src="https://badgen.net/bundlephobia/min/${name}" alt="minified size" />
+      <img src="https://badgen.net/bundlephobia/dependency-count/${name}" alt="dependency count" />`
+    link.href = `https://bundlephobia.com/package/${name}`
+    link.target = '_blank'
+    link.style = 'position: absolute; right: 0.8rem; top: 0.9rem; z-index: 1000; display: flex; gap: 1rem;'
     const anchor = document.querySelector('main article h1') || document.querySelector('#readme') || document.body
     anchor.style.position = 'relative'
-    anchor.append(image)
+    anchor.append(link)
   }
   const detectName = () => {
     let text = document.body.textContent
     if (text.includes('npm') === false) return utils.log('does not seems like the good place to add a badge')
     const copyBlock = utils.findOne('.lh-copy span')
     if (copyBlock) text = copyBlock.textContent + '\n' + text
-    const name = (text.match(/\b(npm i|npm install|npx|yarn add).* ([^-][\w./@-]+)/) || [])[2]
+    const name = (text.match(/\b(npm i|npm install|npx|yarn add).* ([^-][\w./@-]+)/) || text.match(/(unpkg\.com)\/(@?[\w./-]+)/) || [])[2]
     if (name === undefined) return utils.warn('failed to find a npm package name in this page')
     utils.log('found package name :', name)
     injectBadge(name)
