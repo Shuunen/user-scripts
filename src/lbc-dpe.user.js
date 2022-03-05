db6b0bc8af3dda684300174b885fc3c08d47893a
Romain Racamier-Lafon <romain.racamier@gmail.com>
2022-03-05 14:32:24 +0100

    enhance lbc dpe

--- src/lbc-dpe.user.js
+++ src/lbc-dpe.user.js
@@ -6,7 +6,7 @@
 // @match       https://www.leboncoin.fr/*
 // @grant       none
 // @require     https://raw.githubusercontent.com/Shuunen/user-scripts/master/src/utils.js
-// @version     1.0.0
+// @version     1.0.1
 // ==/UserScript==
 
 (function LeBonCoinDPE () {
@@ -15,11 +15,32 @@
   const cls = {
     marker: utils.app.id + '-processed',
   }
+  const districts = {
+    3_001_199: 'Cronenbourg',
+    3_001_211: 'Hautepierre',
+    3_001_183: 'Meinau',
+    3_001_201: 'Stockfeld',
+    3_001_187: 'Cathédrale',
+    3_001_181: 'Neudorf centre',
+    3_001_203: 'Petite France',
+    100_102: 'Illkirch centre ouest',
+    67_218: 'Illkirch',
+  }
+  utils.log(districts)
   const addDpeInfo = (element, name, value, top) => {
     const color = /[a-c]/.test(value) ? 'green' : (/d/.test(value) ? 'orange' : 'red')
     const line = document.createElement('div')
     line.textContent = `${name} : ${value.toUpperCase()}`
     line.style = `color: ${color}; top: ${top}px; font-weight: bold; position: absolute; right: 0;`
+    element.append(line)
+  }
+  const addLocationInfo = (element, ad, top) => {
+    const districtId = ad.attributes.find(a => a.key === 'district_id').value
+    const district = districts[districtId] || districtId
+    if (['Cronenbourg', 'Hautepierre'].includes(district)) return element.style.display = 'none'
+    const line = document.createElement('div')
+    line.textContent = district
+    line.style = `top: ${top}px; font-weight: bold; position: absolute; right: 0;`
     element.append(line)
   }
   const processAd = ad => {
@@ -28,15 +49,19 @@
     if (!link) return document.location.reload() // we need to have that next data in page
     const element = link.parentElement
     if (element.classList.contains(cls.marker)) return utils.log('ad already processed', id)
+    utils.log('process ad :', ad)
     element.classList.add(cls.marker)
     const energy = ad.attributes.find(a => a.key === 'energy_rate').value
     const ges = ad.attributes.find(a => a.key === 'ges').value
-    if (!/[a-d]/.test(energy) || !/[a-d]/.test(ges)) element.style.display = 'none'
+    if (!/[a-c]/.test(energy) || !/[a-c]/.test(ges)) element.style.display = 'none'
     addDpeInfo(element, 'Classe', energy, 25)
     addDpeInfo(element, 'GES', ges, 45)
+    addLocationInfo(element, ad, 65)
   }
   const process = async () => {
-    const { ads } = JSON.parse(document.querySelector('#__NEXT_DATA__').innerHTML).props.pageProps.searchData
+    const { props } = JSON.parse(document.querySelector('#__NEXT_DATA__').innerHTML)
+    if (!props.pageProps || !props.pageProps.searchData) return utils.log('no page props data to parse')
+    const { ads } = props.pageProps.searchData
     utils.log(`processing ${ads.length} ads listing...`)
     ads.forEach(ad => processAd(ad))
   }
