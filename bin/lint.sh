6f850a055471cc21193f1174d0ca07beba1de59d
Romain Racamier-Lafon <romain.racamier@gmail.com>
2023-08-27 11:04:53 +0200

    bump deps

---
+++ bin/lint.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+echo These files should not use user-scripts/src directly :
+
+grep -R user-scripts/src src; if [ $? -eq 1 ]; then echo lint:master success; else echo Use this instead : https://cdn.jsdelivr.net/gh/Shuunen/user-scripts@master/src/utils.js && exit 1; fi
+
+echo These files should not use githubusercontent directly :
+
+grep -R githubusercontent src; if [ $? -eq 1 ]; then echo lint:gh-content success; else echo Migrate to https://www.jsdelivr.com/github && exit 1; fi
