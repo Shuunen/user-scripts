d2b2974fb01019bab9740f5cd73a589f5d6a3d47
Romain Racamier-Lafon <romain.racamier@gmail.com>
2025-01-08 00:00:18 +0100

    feat(utils): enhance toast notification styles and add info type support

--- src/utils.js
+++ src/utils.js
@@ -23,21 +23,19 @@ class Shuutils {
   }
   /**
    * Adds a toast notification to the document body.
-   * @param {'success'|'error'} type - The type of the toast notification.
+   * @param {'success'|'error'|'info'} type - The type of the toast notification.
    * @param {string} message - The message to be displayed in the toast notification.
    * @param {number} delay - The delay in milliseconds before the toast notification is removed.
    * @param {number} padding - The padding in pixels between the toast and the bottom of the window.
    * @example utils.#toastAdd('error', 'hello world', 4000, 20)
    */
-  // eslint-disable-next-line max-params, max-statements
+  // eslint-disable-next-line max-params
   #toastAdd (type, message, delay = 0, padding = 14) {
     const element = document.createElement('div')
     element.setAttribute('class', 'shu-toast')
     const last = document.querySelector('.shu-toast:nth-last-child(1 of .shu-toast)')?.getBoundingClientRect().top
     const bottom = last === undefined ? 0 : globalThis.innerHeight - last
-    const backgrounds = type === 'success' ? ['MediumSeaGreen', 'SeaGreen'] : ['FireBrick', 'Brown']
-    const icon = type === 'success' ? '&check;' : '&cross;'
-    const iconStyle = type === 'success' ? 'line-height: 21px; text-indent: 1px;' : 'line-height: 21.5px;' // @ts-expect-error it works (๑◕ܫ◕๑)
+    const { backgrounds, icon, iconStyle } = this.#toastStyle(type) // @ts-expect-error it works (๑◕ܫ◕๑)
     element.style = `position: fixed; display: flex; align-items: center; gap: 9px; bottom: ${bottom + padding}px; right: ${padding}px; z-index: 99999; padding: 12px 20px 11px 14px; background: linear-gradient(45deg, ${backgrounds[0]}, 20%, ${backgrounds[1]}); color: white; border-radius: 5px; box-shadow: 0 3px 7px 0 rgba(0,0,0,.5); font-size: 18px; opacity: 0; transition: opacity 0.3s ease-in-out, transform 0.3s ease-in-out; transform: translateX(300px);`
     element.innerHTML = `<span style="${iconStyle}border-radius: 50%; color: ${backgrounds[1]}; background-color: #ffffff90; width: 20px; height: 20px; text-align: center; font-weight: bolder; font-size: 12px;">${icon}</span><span style="margin-top: -1px;">${message}</span>`
     this.#toastShow(element)
@@ -64,6 +62,16 @@ class Shuutils {
       element.style.opacity = '1'
       element.style.transform = 'translateX(0)'
     }, delay)
+  }
+  /**
+   * Get the style for a toast notification.
+   * @param {'success'|'error'|'info'} type - The type of the toast notification.
+   * @returns {{backgrounds: string[], icon: string, iconStyle: string}} The style for the toast notification.
+   */
+  #toastStyle (type) {
+    if (type === 'success') return { backgrounds: ['MediumSeaGreen', 'SeaGreen'], icon: '&check;', iconStyle: 'line-height: 21px; text-indent: 1px;' }
+    if (type === 'error') return { backgrounds: ['FireBrick', 'Brown'], icon: 'x', iconStyle: 'line-height: 21.5px;' }
+    return { backgrounds: ['DodgerBlue', 'RoyalBlue'], icon: 'i', iconStyle: 'line-height: 21px;' }
   }
   /**
    * Animate an element with animate.css
@@ -460,6 +468,13 @@ class Shuutils {
    * @example utils.toastError('hello world')
    */
   toastError (message, delay = 4000) { this.#toastAdd('error', message, delay) }
+  /**
+   * Displays an info toast message.
+   * @param {string} message - The info message to display.
+   * @param {number} [delay] - The delay in milliseconds before the toast disappears. Default is 3000ms.
+   * @example utils.toastInfo('hello world')
+   */
+  toastInfo (message, delay = 2000) { this.#toastAdd('info', message, delay) }
   /**
    * Displays a success toast message.
    * @param {string} message - The success message to display.
