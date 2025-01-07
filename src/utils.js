64dfe4a5139d1fd7c991b4a898ebd0867bc4afc7
Romain Racamier-Lafon <romain.racamier@gmail.com>
2025-01-07 23:37:59 +0100

    fix(utils): improve toast icon styling and handle undefined last element

--- src/utils.js
+++ src/utils.js
@@ -34,10 +34,10 @@ class Shuutils {
     const element = document.createElement('div')
     element.setAttribute('class', 'shu-toast')
     const last = document.querySelector('.shu-toast:nth-last-child(1 of .shu-toast)')?.getBoundingClientRect().top
-    const bottom = last ? globalThis.innerHeight - last : 0
+    const bottom = last === undefined ? 0 : globalThis.innerHeight - last
     const backgrounds = type === 'success' ? ['MediumSeaGreen', 'SeaGreen'] : ['FireBrick', 'Brown']
-    const icon = type === 'success' ? '✔' : '✖' // @ts-ignore it works (๑◕ܫ◕๑)
-    const iconStyle = type === 'success' ? 'line-height: 21px; text-indent: 1px;' : 'line-height: 21.5px;' // @ts-ignore it works (๑◕ܫ◕๑)
+    const icon = type === 'success' ? '&check;' : '&cross;'
+    const iconStyle = type === 'success' ? 'line-height: 21px; text-indent: 1px;' : 'line-height: 21.5px;' // @ts-expect-error it works (๑◕ܫ◕๑)
     element.style = `position: fixed; display: flex; align-items: center; gap: 9px; bottom: ${bottom + padding}px; right: ${padding}px; z-index: 99999; padding: 12px 20px 11px 14px; background: linear-gradient(45deg, ${backgrounds[0]}, 20%, ${backgrounds[1]}); color: white; border-radius: 5px; box-shadow: 0 3px 7px 0 rgba(0,0,0,.5); font-size: 18px; opacity: 0; transition: opacity 0.3s ease-in-out, transform 0.3s ease-in-out; transform: translateX(300px);`
     element.innerHTML = `<span style="${iconStyle}border-radius: 50%; color: ${backgrounds[1]}; background-color: #ffffff90; width: 20px; height: 20px; text-align: center; font-weight: bolder; font-size: 12px;">${icon}</span><span style="margin-top: -1px;">${message}</span>`
     this.#toastShow(element)
